8a6cec45a00235c41567ef6f8d304603fe9a3de4
Nachiket Kakatkar <nachiket.kakatkar@drillinginfo.com>
2013-07-19 19:34:46 +0530

    Added courses dummy data. To be used in the api.

---
+++ app/dummyData/courses.js
@@ -0,0 +1,75 @@
+module.export = {
+  data: [
+    {
+      title: 'Introduction to NodeJS',
+      author: ['Ryan Dahl'],
+      startDate: '01-01-2013',
+      endDate: '21-02-2013',
+      code: 'NJS101',
+      category: ['TechNext']
+    },{
+      title: 'Advanced Node',
+      author: ['Ryan Dahl', 'Isaac Schuetler'],
+      startDate: '28-02-2013',
+      endDate: '07-04-2013',
+      code: 'NJS202',
+      category: ['TechNext']
+    },{
+      title: 'NodejS and MongoDB',
+      author: ['Hussain Pithawala', 'Alok Guha'],
+      startDate: '14-04-2013',
+      endDate: '01-06-2013',
+      code: 'NJS303',
+      category: ['TechNext', 'Databases']
+    },{
+      title: 'Introduction to BackboneJS',
+      author: ['Rohit Ghatol', 'Addy Osmani'],
+      startDate: '01-03-2013',
+      endDate: '08-03-2013',
+      code: 'JSF301',
+      category: ['TechNext', 'Frameworks', 'JavaScript']
+    },{
+      title: 'Introduction to Aura',
+      author: ['Rohit Ghatol', 'Addy Osmani'],
+      startDate: '15-03-2013',
+      endDate: '22-03-2013',
+      code: 'JSF301',
+      category: ['TechNext', 'Frameworks', 'JavaScript']
+    },{
+      title: 'Introduction to SASS',
+      author: ['Rohit Ghatol'],
+      startDate: '23-03-2013',
+      endDate: '30-03-2013',
+      code: 'CSS301',
+      category: ['TechNext','CSS']
+    },{
+      title: 'Advanced JavaScript',
+      author: ['Douglas Crockford'],
+      startDate: '01-08-2013',
+      endDate: '31-08-2013',
+      code: 'JS505',
+      category: ['JavaScript']
+    },{
+      title: 'Basics of HTML5 & CSS3',
+      author: ['Rohit Ghatol'],
+      startDate: '01-09-2013',
+      endDate: '03-09-2013',
+      code: 'TN101',
+      category: ['TechNext', 'HTML5', 'CSS3']
+    },{
+      title: 'Introduction to D3',
+      author: ['Rohit Ghatol'],
+      startDate: '08-09-2013',
+      endDate: '22-09-2013',
+      code: 'D3JS101',
+      category: ['Visualization', 'JavaScript', 'Graphics']
+    },{
+      title: 'Introduction to ThreeJS',
+      author: ['Rohit Ghatol'],
+      startDate: '29-09-2013',
+      endDate: '14-09-2013',
+      code: 'THJS101',
+      category: ['Visualization', 'JavaScript', 'Graphics']
+    }
+  ]
+};
